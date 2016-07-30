0d5b00123054de7eccf53462f1d02d1726693eeb
Matthew Goldwater <mattgwater@gmail.com>
2016-07-29 19:37:45 -0700

    toggle strict light

--- main.js
+++ main.js
@@ -2,6 +2,14 @@ function on(){
   $(".on").css("background", "#6495ed");
   $(".off").css("background", "#222");
   $(".count-display").text("--");
+  // return function enableStuff(){
+    $(".start-button").on("click", function(){
+    start();
+  });
+    $(".strict-button").on("click", function(){
+      toggleStrict();
+    })
+  // };
 }
 
 function off(){
@@ -17,3 +25,24 @@ $(".on").on("click",function(){
 $(".off").on("click",function(){
   off();
 })
+
+function start(){
+  console.log("hits start button");
+}
+
+function toggleStrict(){
+  console.log("hits strict button");
+  var backgroundColor = $(".strict-light").css("background-color");
+  // console.log(backgroundColor);
+  if (backgroundColor == "rgb(34, 34, 34)") {
+    // console.log("strict-light background is almost black")
+    $(".strict-light").css("background", "red");
+    enterStrictMode(); 
+  } else {
+    $(".strict-light").css("background", "#222");
+  } // strict light is red and strict mode is on
+}
+
+function enterStrictMode(){
+  // console.log("in enterStrictMode function");
+}
