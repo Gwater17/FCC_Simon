b2ba1d0906a3a73874459203bd897b1a5c71481c
Matthew Goldwater <mattgwater@gmail.com>
2016-08-01 18:02:01 -0700

    plays sequence in order, but current sequence goes too fast

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ function newGame(){
 }
 
 function computerGivesInstructions(sequence, count){
-  console.log("computerGivesInstructions executes");
+  console.log("computerGivesInstructions executes", sequence, count);
   if (count === 20){
     $(".count-display").text("You win");
     // setTimeout(incrementCount,1000);
@@ -49,42 +49,87 @@ function computerGivesInstructions(sequence, count){
   }
   count++;
   $(".count-display").text(count);
+  playCurrentSequence(sequence);
   var randomNum = Math.random();
   if (randomNum > .75) {
-    playButtonSound("red");
+    setTimeout(function(){
+      playButtonSound("red");
+    },1000);
+    sequence.push("red");
   } else if (randomNum > .5) {
-    playButtonSound("green");
+    setTimeout(function(){
+      playButtonSound("green");
+    },1000)
+    sequence.push("green");
   } else if (randomNum > .25) {
-    playButtonSound("blue");
+    setTimeout(function(){
+      playButtonSound("blue");
+    },1000)
+    sequence.push("blue");
   } else { //randomNum
-    playButtonSound("yellow");
+    setTimeout(function(){
+      playButtonSound("yellow");
+    },1000)
+    sequence.push("yellow");
   }
   userInput(sequence,count);
 }
 
 function userInput(colorOrder,currNum){
+  console.log(colorOrder, currNum);
   var userClickOrder = [];
   console.log("userInput function runs")
   $(".red-button").on("click",function(){
     playButtonSound("red");
     userClickOrder.push("red");
     console.log(userClickOrder);
+    //create function after each click
+    isUserCorrect(colorOrder, userClickOrder);
   })
   $(".green-button").on("click",function(){
     playButtonSound("green");
     userClickOrder.push("green");
-    console.log(userClickOrder);
+    console.log(userClickOrder, colorOrder);
+    isUserCorrect(colorOrder, userClickOrder);
   })
   $(".blue-button").on("click", function(){
     playButtonSound("blue");
     userClickOrder.push("blue");
-    console.log(userClickOrder);
+    // console.log(userClickOrder);
+    isUserCorrect(colorOrder, userClickOrder);
   })
   $(".yellow-button").on("click", function(){
     playButtonSound("yellow");
     userClickOrder.push("yellow");
-    console.log(userClickOrder);
+    // console.log(userClickOrder);
+    isUserCorrect(colorOrder, userClickOrder);
   })
+}
+
+function playCurrentSequence(existingSequence){
+  console.log("hits the playCurrentSequence function", existingSequence);
+  for (var i = 0; i < existingSequence.length; i++){
+    (function IIFE(currColor){
+      playButtonSound(currColor);
+    })(existingSequence[i])
+  }
+  // existingSequence.forEach(function(value){
+    // playButtonSound("value");
+  // })
+}
+
+/*need conditions for...
+  //1. if the computerOrder and userOrder is the same up to the point of the computerOrders length. (so userOrder is the same as computerOrder)
+  //2. if the computerOrder and userOrder is the same for the current slice, but the user has not finished guessing the sequence
+  //3. if the user guesses incorrectly meaning that the current slice of the userOrder and the computerOrder does not match*/
+function isUserCorrect(computerOrder, userOrder){
+  console.log("hits isUserCorrect function", computerOrder, userOrder);
+  if (computerOrder.length === userOrder.length && computerOrder[computerOrder.length -1] === userOrder[userOrder.length-1]){
+    console.log("hits this if statement", computerOrder, userOrder);
+    return setTimeout(function(){
+      computerGivesInstructions(computerOrder, computerOrder.length);
+    },1000)
+  }
 }
 
 function playButtonSound(color){
@@ -94,7 +139,7 @@ function playButtonSound(color){
     audio.play();
     $(".red-button").css("opacity", "1");
     setInterval(function(){
-      $(".red-button").css("opacity", "0.5");
+      $(".red-button").css("opacity", "0.75");
   }, 1000)
   } else if (color === "green") {
     var audio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3');
@@ -126,7 +171,7 @@ function toggleStrict(){
   console.log("hits strict button");
   var backgroundColor = $(".strict-light").css("background-color");
   // console.log(backgroundColor);
-  if (backgroundColor == "rgb(34, 34, 34)") {
+  if (backgroundColor == "rgb(34, 34, 34)") { //same as #222
     // console.log("strict-light background is almost black")
     $(".strict-light").css("background", "red");
     // enterStrictMode(); 
